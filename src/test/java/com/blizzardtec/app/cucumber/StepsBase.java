fa6e47161b897a59ecf19e16b8d99e328be30032
barnabyg <barnabyg@blizzardtec.com>
2014-06-04 19:15:41 +0100

    changed port

--- src/test/java/com/blizzardtec/app/cucumber/StepsBase.java
+++ src/test/java/com/blizzardtec/app/cucumber/StepsBase.java
@@ -33,7 +33,7 @@ public class StepsBase {
     /**
      * Base URL.
      */
-    public static final String BASE_URL = "http://localhost:8080/";
+    public static final String BASE_URL = "http://localhost:7070/";
     /**
      * Window height.
      */
